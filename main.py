78862533924ed5a53f404a530fb7c047ef4980e5
Maikruts Ilya Oleksandrovych <143324723+1337rave@users.noreply.github.com>
2023-10-05 21:01:25 +0300

    ex 1 was done

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+import re
 
+def process_text(input_text):
+
+    sentences = re.split(r'(?<=[.!?])\s+', input_text)
+    capitalized_sentences = [sentence.capitalize() for sentence in sentences]
+    processed_text = ' '.join(capitalized_sentences)
+
+    digit_count = sum(1 for char in processed_text if char.isdigit())
+
+    punctuation_count = sum(1 for char in processed_text if char in '.,;!?')
+
+    exclamation_count = processed_text.count('!')
+
+    return processed_text, digit_count, punctuation_count, exclamation_count
+
+input_text = "не важно о чем. тоже не важно! і 123, ок 5."
+
+processed_text, digit_count, punctuation_count, exclamation_count = process_text(input_text)
+print("Змінений текст:")
+print(processed_text)
+print("Кількість цифр у тексті:", digit_count)
+print("Кількість розділових знаків у тексті:", punctuation_count)
+print("Кількість знаків оклику у тексті:", exclamation_count)
