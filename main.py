7a39842e3ad30944726afbbc88bd07698fc012bd
Maikruts Ilya Oleksandrovych <143324723+1337rave@users.noreply.github.com>
2023-10-05 21:05:26 +0300

    ex 2 was done

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-
+input_numbers = input("Введіть елементи списку цілих чисел, розділені пробілами: ")
+numbers = [int(num) for num in input_numbers.split()]
+target_number = int(input("Введіть число для пошуку: "))
+count = numbers.count(target_number)
+print(f"Число {target_number} зустрічається в списку {count} разів.")
