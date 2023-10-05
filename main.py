41f6e300c21c658d778abeb24583bbbc67819d64
Maikruts Ilya Oleksandrovych <143324723+1337rave@users.noreply.github.com>
2023-10-05 21:17:16 +0300

    ex 3 was done

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-
+input_numbers = input("Введіть елементи списку цілих чисел, розділені пробілом: ")
+numbers = [int(num) for num in input_numbers.split()]
+total_sum = (sum(numbers))
+average = total_sum / len(numbers)
+print(f"Сума всіх елементів у списку: {total_sum}")
+print(f"Середнє арифметичне елементів у списку: {average}")
